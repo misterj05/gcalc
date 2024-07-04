6921c93c9f49449f20aac045173651012705547e
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-07-04 00:32:38 -0400

    added versioning

--- gcalc.go
+++ gcalc.go
@@ -9,15 +9,16 @@ import (
 	"runtime"
 )
 
-func clearTerm() {
-	white := color.FgWhite.Render
-	blue := color.FgLightBlue.Render
-	lightYellow := color.FgLightYellow.Render
+const ver float32 = 0.61
 
-	// fmt.Print("\033[H\033[2J")
+func clearTerm() {
+	white := color.New(color.FgWhite, color.BgBlack).Render
+	blue := color.New(color.FgLightBlue, color.BgBlack).Render
+	lightYellow := color.New(color.FgLightYellow, color.BgBlack).Render
+
 	switch runtime.GOOS {
 	case "linux":
-		_, errLinuxClearTerm := fmt.Println("\033[H\033[2J\033[34mGCalc \033[97mby \033[35mWideHardo J\n\033[33mA simple terminal calculator written in \033[34mGo\033[33m.\nFormatting goes {number operation number}.\n\033[0m")
+		_, errLinuxClearTerm := fmt.Println("\033[H\033[2J\033[34mGCalc", ver, "\033[97mby \033[35mWideHardo J\n\033[33mA simple terminal calculator written in \033[34mGo\033[33m.\nFormatting goes {number operation number}.\n\033[0m")
 		// _, errLinuxClearTerm := fmt.Print(blue("GCalc by "), purple("WideHardo J\n"), yellow("A simple terminal calculator written in "), blue("Go"), yellow(".\nFormatting goes {number operation number}.\n"))
 		if errLinuxClearTerm != nil {
 			log.Fatal("linuxPrintErr: ", errLinuxClearTerm)
@@ -26,12 +27,12 @@ func clearTerm() {
 		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
-		_, errWindowsClearTerm := fmt.Print(blue("GCalc "), white("by "), blue("WideHardo J\n"), lightYellow("A simple terminal calculator written in "), blue("Go"), lightYellow(".\nFormatting goes {number operation number}.\n"))
+		_, errWindowsClearTerm := fmt.Print(blue("GCalc "), white(ver, "by "), blue("WideHardo J\n"), lightYellow("A simple terminal calculator written in "), blue("Go"), lightYellow(".\nFormatting goes {number operation number}.\n"))
 		if errWindowsClearTerm != nil {
 			log.Fatal("defaultPrintErr: ", errWindowsClearTerm)
 		}
 	default:
-		_, errDefaultPrint := fmt.Println("GCalc by WideHardo J\nA simple terminal calculator written in Go.\nFormatting goes {number operation number}.\n")
+		_, errDefaultPrint := fmt.Println("GCalc ", ver, "by WideHardo J\nA simple terminal calculator written in Go.\nFormatting goes {number operation number}.\n")
 		if errDefaultPrint != nil {
 			log.Fatal("defaultPrintErr: ", errDefaultPrint)
 		}
