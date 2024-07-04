f81274ecb9752bc0c1734874073a73009c2a480b
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-07-04 15:31:28 -0400

    added time.Sleep

--- gcalc.go
+++ gcalc.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/gookit/color"
 	"log"
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
+
+	"github.com/gookit/color"
 )
 
 const ver float32 = 0.63
@@ -21,7 +23,8 @@ func clearTerm() {
 		_, errLinuxClearTerm := fmt.Println("\033[H\033[2J\033[34mGCalc", ver, "\033[97mby \033[35mWideHardo J\n\033[33mA simple terminal calculator written in \033[34mGo\033[33m.\nFormatting goes {number operation number}. Example:2 + 3\n\033[0m")
 		// _, errLinuxClearTerm := fmt.Print(blue("GCalc by "), purple("WideHardo J\n"), yellow("A simple terminal calculator written in "), blue("Go"), yellow(".\nFormatting goes {number operation number}.\n"))
 		if errLinuxClearTerm != nil {
-			log.Fatal("linuxPrintErr: ", errLinuxClearTerm)
+			log.Println("linuxPrintErr: ", errLinuxClearTerm)
+			time.Sleep(2)
 		}
 	case "windows":
 		cmd := exec.Command("cmd", "/c", "cls")
@@ -29,12 +32,14 @@ func clearTerm() {
 		cmd.Run()
 		_, errWindowsClearTerm := fmt.Print(blue("GCalc ", ver), white(" by "), blue("WideHardo J\n"), lightYellow("A simple terminal calculator written in "), blue("Go"), lightYellow(".\nFormatting goes {number operation number}. Example:2 + 3\n"))
 		if errWindowsClearTerm != nil {
-			log.Fatal("defaultPrintErr: ", errWindowsClearTerm)
+			log.Println("defaultPrintErr: ", errWindowsClearTerm)
+			time.Sleep(2)
 		}
 	default:
 		_, errDefaultClearTerm := fmt.Println("GCalc ", ver, "by WideHardo J\nA simple terminal calculator written in Go.\nFormatting goes {number operation number}. Example:2 + 3\n")
 		if errDefaultClearTerm != nil {
-			log.Fatal("defaultPrintErr: ", errDefaultClearTerm)
+			log.Println("defaultPrintErr: ", errDefaultClearTerm)
+			time.Sleep(2)
 		}
 	}
 }
@@ -46,16 +51,19 @@ func main() {
 	for {
 		_, printExpErr := fmt.Println("Give an expression.\n")
 		if printExpErr != nil {
-			log.Fatal("Print Expression Error: ", printExpErr)
+			log.Println("Print Expression Error: ", printExpErr)
+			time.Sleep(2)
 		}
 		_, scanErr := fmt.Scanln(&num1, &operator, &num2)
 		if scanErr != nil {
 			log.Println("Scan Error: ", scanErr)
+			time.Sleep(2)
 		}
 		clearTerm()
 		_, printResultErr := fmt.Printf("%d %s %d = ", num1, operator, num2)
 		if printResultErr != nil {
-			log.Fatal("Print Result Error: ", printResultErr)
+			log.Println("Print Result Error: ", printResultErr)
+			time.Sleep(2)
 		}
 		switch operator {
 		case "+":
