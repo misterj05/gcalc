698d6d591937b926db6428b09fffe97388511d2d
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-07-04 15:41:32 -0400

    err stuff

--- gcalc.go
+++ gcalc.go
@@ -20,25 +20,25 @@ func clearTerm() {
 
 	switch runtime.GOOS {
 	case "linux":
-		_, errLinuxClearTerm := fmt.Println("\033[H\033[2J\033[34mGCalc", ver, "\033[97mby \033[35mWideHardo J\n\033[33mA simple terminal calculator written in \033[34mGo\033[33m.\nFormatting goes {number operation number}. Example:2 + 3\n\033[0m")
+		_, errLinuxClearTerm := fmt.Println("\033[H\033[2J\033[34mGCalc", ver, "\033[97mby \033[35mWideHardo J\n\033[33mA simple terminal calculator written in \033[34mGo\033[33m.\nFormatting goes {number operation number}. Example:2 + 3\033[0m")
 		// _, errLinuxClearTerm := fmt.Print(blue("GCalc by "), purple("WideHardo J\n"), yellow("A simple terminal calculator written in "), blue("Go"), yellow(".\nFormatting goes {number operation number}.\n"))
 		if errLinuxClearTerm != nil {
-			log.Println("linuxPrintErr: ", errLinuxClearTerm)
+			log.Println("linuxClearTermPrintErr: ", errLinuxClearTerm)
 			time.Sleep(2)
 		}
 	case "windows":
 		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
-		_, errWindowsClearTerm := fmt.Print(blue("GCalc ", ver), white(" by "), blue("WideHardo J\n"), lightYellow("A simple terminal calculator written in "), blue("Go"), lightYellow(".\nFormatting goes {number operation number}. Example:2 + 3\n"))
+		_, errWindowsClearTerm := fmt.Print(blue("GCalc ", ver), white(" by "), blue("WideHardo J\n"), lightYellow("A simple terminal calculator written in "), blue("Go"), lightYellow(".\nFormatting goes {number operation number}. Example:2 + 3"))
 		if errWindowsClearTerm != nil {
-			log.Println("defaultPrintErr: ", errWindowsClearTerm)
+			log.Println("windowsClearTermPrintErr: ", errWindowsClearTerm)
 			time.Sleep(2)
 		}
 	default:
-		_, errDefaultClearTerm := fmt.Println("GCalc ", ver, "by WideHardo J\nA simple terminal calculator written in Go.\nFormatting goes {number operation number}. Example:2 + 3\n")
+		_, errDefaultClearTerm := fmt.Println("GCalc ", ver, "by WideHardo J\nA simple terminal calculator written in Go.\nFormatting goes {number operation number}. Example:2 + 3")
 		if errDefaultClearTerm != nil {
-			log.Println("defaultPrintErr: ", errDefaultClearTerm)
+			log.Println("defaultClearTermPrintErr: ", errDefaultClearTerm)
 			time.Sleep(2)
 		}
 	}
@@ -49,20 +49,20 @@ func main() {
 	var operator string
 	clearTerm()
 	for {
-		_, printExpErr := fmt.Println("Give an expression.\n")
-		if printExpErr != nil {
-			log.Println("Print Expression Error: ", printExpErr)
+		_, errPrintExp := fmt.Println("Give an expression.")
+		if errPrintExp != nil {
+			log.Println("Print Expression Error: ", errPrintExp)
 			time.Sleep(2)
 		}
-		_, scanErr := fmt.Scanln(&num1, &operator, &num2)
-		if scanErr != nil {
-			log.Println("Scan Error: ", scanErr)
+		_, errScan := fmt.Scanln(&num1, &operator, &num2)
+		if errScan != nil {
+			log.Println("Scan Error: ", errScan)
 			time.Sleep(2)
 		}
 		clearTerm()
-		_, printResultErr := fmt.Printf("%d %s %d = ", num1, operator, num2)
-		if printResultErr != nil {
-			log.Println("Print Result Error: ", printResultErr)
+		_, errPrintResult := fmt.Printf("%d %s %d = ", num1, operator, num2)
+		if errPrintResult != nil {
+			log.Println("Print Result Error: ", errPrintResult)
 			time.Sleep(2)
 		}
 		switch operator {
